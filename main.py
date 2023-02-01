6de98aa02367e44599c380e88071976d7c2ef989
William He <contact@williamhe.dev>
2023-01-31 22:43:53 -0800

    Added login

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sqlite3
+
 from fastapi import FastAPI
 from pydantic import BaseModel
-import sqlite3
 
 
 class NewAccount(BaseModel):
@@ -13,46 +14,83 @@ class GetAccount(BaseModel):
     username: str
 
 
+class LoginAccount(BaseModel):
+    email: str
+    password: str
+
+
 app = FastAPI()
-con = sqlite3.connect("accounts.db")
-cur = con.cursor()
-
-
 base_route = "/api/v1"
 
 
 @app.post(base_route + "/login")
-def login():
-    return {
-        "success": True
-    }
+def login(account: LoginAccount):
+    with sqlite3.connect("accounts.db") as con:
+        cur = con.cursor()
+        query = f"SELECT username " \
+                f"FROM accounts " \
+                f"WHERE email='{account.email}' " \
+                f"AND password='{account.password}'"
+        res = cur.execute(query)
+
+        if len(res.fetchall()) > 0:
+            return {
+                "success": True,
+                "auth_token": "example_auth_token"
+            }
+        else:
+            return {
+                "success": False
+            }
 
 
 @app.post(base_route + "/signup")
 async def signup(account: NewAccount):
-    query = "SELECT count(*) FROM accounts WHERE email='" + account.email + "' OR username='" + account.username + "'"
-    res = cur.execute(query)
+    with sqlite3.connect("accounts.db") as con:
+        # Encrypt email
+        hashed_email = account.email
+        # Encrypt password
+        hashed_password = account.password
 
-    if res.fetchone()[0] > 0:
+        cur = con.cursor()
+        query = f"SELECT count(*) " \
+                f"FROM accounts " \
+                f"WHERE email='{hashed_email}' " \
+                f"OR username='{account.username}' "
+        res = cur.execute(query)
+
+        if res.fetchone()[0] > 0:
+            return {
+                "success": False,
+                "message": "An account already exists with this email or username."
+            }
+
+        print(f"hashed_email: {hashed_email}")
+        print(f"hashed_password: {hashed_password}")
+
+        query = f"INSERT INTO accounts (email, username, password)" \
+                f" VALUES (" \
+                f"'{account.username}'," \
+                f"'{hashed_email}'," \
+                f"'{hashed_password}'" \
+                f")"
+        res = cur.execute(query)
+
         return {
-            "success": False,
-            "message": "An account already exists with either this username or password."
+            "success": True,
         }
 
-    query = "INSERT INTO accounts (username, password, email) VALUES ('" + account.username + "', '" + account.password + "', '" + account.email + "') "
-    res = cur.execute(query)
 
-    return {
-        "success": True
-    }
+@app.post(base_route + "/getAccount")
+async def get_account(account: GetAccount):
+    with sqlite3.connect("accounts.db") as con:
+        cur = con.cursor()
+        query = f"SELECT username " \
+                f"FROM accounts " \
+                f"WHERE username='{account.username}'"
+        res = cur.execute(query)
 
-
-@app.post(base_route + "/account")
-def get_account(account: GetAccount):
-    query = "SELECT count(*) FROM accounts WHERE username='" + account.username + "'"
-    res = cur.execute(query)
-
-    return {
-        "success": True,
-        "data": res.fetchone()[0]
-    }
+        return {
+            "success": True,
+            "data": res.fetchall()
+        }
